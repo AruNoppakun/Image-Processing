13037a2923577b8c6e6e42428c94b51f2cce745f
AruNoppakun <16532096@aru.ac.th>
2025-05-29 14:40:08 +0700

    Update prompt.py

--- prompt.py
+++ prompt.py
@@ -3,7 +3,7 @@ from PIL import Image
 import requests
 from io import BytesIO
 
-# URLs ของภาพแบบ set หรือ list
+# URLs ของภาพแบบ list
 image_urls = [
     "https://upload.wikimedia.org/wikipedia/commons/b/bf/Bulldog_inglese.jpg",
     "https://f.ptcdn.info/002/048/000/oidav7m4nF0JGvXz44Y-o.jpg",
@@ -17,4 +17,4 @@ cols = st.columns(3)
 for col, url in zip(cols, image_urls):
     response = requests.get(url)
     img = Image.open(BytesIO(response.content))
-    col.image(img, use_column_width=True)
+    col.image(img, use_container_width=True)
