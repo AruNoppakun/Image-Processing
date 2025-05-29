debfd47ad0494a4c427c8a94c89357ed88d63c6e
AruNoppakun <16532096@aru.ac.th>
2025-05-29 14:37:02 +0700

    Update prompt.py

--- prompt.py
+++ prompt.py
@@ -6,8 +6,8 @@ from io import BytesIO
 # URLs ของภาพทั้ง 3
 image_urls = {
     "Bulldog (หมา)": "https://upload.wikimedia.org/wikipedia/commons/b/bf/Bulldog_inglese.jpg",
-    "Cat (แมว)": "https://upload.wikimedia.org/wikipedia/commons/3/3a/Cat03.jpg",
-    "Monkey (ลิง)": "https://upload.wikimedia.org/wikipedia/commons/e/e3/Capuchin_Costa_Rica_03.jpg"
+    "Cat (แมว)": "https://f.ptcdn.info/002/048/000/oidav7m4nF0JGvXz44Y-o.jpg",
+    "Monkey (ลิง)": "https://themomentum.co/wp-content/uploads/2020/02/%E0%B8%AA%E0%B8%B1%E0%B8%95%E0%B8%A7%E0%B9%8C%E0%B8%9B%E0%B9%88%E0%B8%B2-Thumbnail.png"
 }
 
 st.title("แสดงภาพสัตว์สามชนิด")
